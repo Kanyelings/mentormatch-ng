c0b349337584b13e0b9a14df9e891fd5b8c67bfb
elroykanye <elroykanye@gmail.com>
2021-10-21 13:40:16 +0100

    setup components : modified ./src/app/components/contact/contact-main/contact-main.component.ts

---
+++ src/app/components/contact/contact-main/contact-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-main',
+  templateUrl: './contact-main.component.html',
+  styleUrls: ['./contact-main.component.scss']
+})
+export class ContactMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
