123403b654f2efc92b7f565cda65b6850387d757
elroykanye <elroykanye@gmail.com>
2021-10-21 13:40:12 +0100

    setup components : modified ./src/app/components/home/home-mentors/home-mentors.component.ts

---
+++ src/app/components/home/home-mentors/home-mentors.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-mentors',
+  templateUrl: './home-mentors.component.html',
+  styleUrls: ['./home-mentors.component.scss']
+})
+export class HomeMentorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
