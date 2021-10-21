892a9f75a3f46d0ff7c674bdfc6a98ad4f1d3403
elroykanye <elroykanye@gmail.com>
2021-10-21 13:40:13 +0100

    setup components : modified ./src/app/components/mentor-mentee/mm-mentor-form/mm-mentor-form.component.ts

---
+++ src/app/components/mentor-mentee/mm-mentor-form/mm-mentor-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mm-mentor-form',
+  templateUrl: './mm-mentor-form.component.html',
+  styleUrls: ['./mm-mentor-form.component.scss']
+})
+export class MmMentorFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
