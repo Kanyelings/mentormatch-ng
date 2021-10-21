ba31e92e230a87ad3dc183cd20aa4c85bee23830
elroykanye <elroykanye@gmail.com>
2021-10-21 13:40:13 +0100

    setup components : modified ./src/app/components/home/home-hero/home-hero.component.ts

---
+++ src/app/components/home/home-hero/home-hero.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-hero',
+  templateUrl: './home-hero.component.html',
+  styleUrls: ['./home-hero.component.scss']
+})
+export class HomeHeroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
