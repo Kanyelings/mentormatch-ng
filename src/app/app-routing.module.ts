17c377ace1c7034c342c8357041facff0044f1d9
Elroy Kanye <68631069+elroykanye@users.noreply.github.com>
2021-10-21 13:48:46 +0100

    Revert " resolves #3"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {MainHomeComponent} from "./components/home/main-home/main-home.component";
-import {ContactMainComponent} from "./components/contact/contact-main/contact-main.component";
-import {MmMentorFormComponent} from "./components/mentor-mentee/mm-mentor-form/mm-mentor-form.component";
-import {MmMenteeFormComponent} from "./components/mentor-mentee/mm-mentee-form/mm-mentee-form.component";
 
-const routes: Routes = [
-  {
-    path: '', component: MainHomeComponent
-  },
-  {
-    path: 'contact', component: ContactMainComponent
-  },
-  {
-    path: 'mentor-form', component: MmMentorFormComponent
-  },
-  {
-    path: 'mentee-form', component: MmMenteeFormComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,46 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainNavComponent } from './components/nav/main-nav/main-nav.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MainHomeComponent } from './components/home/main-home/main-home.component';
-import { HomeHeroComponent } from './components/home/home-hero/home-hero.component';
-import { HomeAboutComponent } from './components/home/home-about/home-about.component';
-import { HomeCountsComponent } from './components/home/home-counts/home-counts.component';
-import { HomeWhyComponent } from './components/home/home-why/home-why.component';
-import { HomeMentorsComponent } from './components/home/home-mentors/home-mentors.component';
-import { FooterNavComponent } from './components/nav/footer-nav/footer-nav.component';
-import { ToTopNavComponent } from './components/nav/to-top-nav/to-top-nav.component';
-import { ContactMainComponent } from './components/contact/contact-main/contact-main.component';
-import { ContactMapComponent } from './components/contact/contact-map/contact-map.component';
-import { ContactFormComponent } from './components/contact/contact-form/contact-form.component';
-import { MmMainComponent } from './components/mentor-mentee/mm-main/mm-main.component';
-import { MmMentorFormComponent } from './components/mentor-mentee/mm-mentor-form/mm-mentor-form.component';
-import { MmMenteeFormComponent } from './components/mentor-mentee/mm-mentee-form/mm-mentee-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MainNavComponent,
-    MainHomeComponent,
-    HomeHeroComponent,
-    HomeAboutComponent,
-    HomeCountsComponent,
-    HomeWhyComponent,
-    HomeMentorsComponent,
-    FooterNavComponent,
-    ToTopNavComponent,
-    ContactMainComponent,
-    ContactMapComponent,
-    ContactFormComponent,
-    MmMainComponent,
-    MmMentorFormComponent,
-    MmMenteeFormComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
