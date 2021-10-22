df93506f892a25455c467f0e5309789e03702bf2
elroykanye <elroykanye@gmail.com>
2021-10-22 22:50:44 +0100

    setup hero and reg forms : modified ./src/app/app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import {MainHomeComponent} from "./components/home/main-home/main-home.component
 import {ContactMainComponent} from "./components/contact/contact-main/contact-main.component";
 import {MmMentorFormComponent} from "./components/mentor-mentee/mm-mentor-form/mm-mentor-form.component";
 import {MmMenteeFormComponent} from "./components/mentor-mentee/mm-mentee-form/mm-mentee-form.component";
+import {MmMainComponent} from "./components/mentor-mentee/mm-main/mm-main.component";
 
 const routes: Routes = [
   {
@@ -11,6 +12,9 @@ const routes: Routes = [
   },
   {
     path: 'contact', component: ContactMainComponent
+  },
+  {
+    path: 'mm', component: MmMainComponent
   },
   {
     path: 'mentor-form', component: MmMentorFormComponent
