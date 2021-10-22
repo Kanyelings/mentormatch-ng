2014566fef51c790fdb1de662f4d77b9df840c06
elroykanye <elroykanye@gmail.com>
2021-10-22 04:37:56 +0100

    mod to main n

--- src/app/components/nav/main-nav/main-nav.component.ts
+++ src/app/components/nav/main-nav/main-nav.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainNavComponent implements OnInit {
 
+  showMobileNav: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addMobileNav(navBar: HTMLElement, navIcon: HTMLElement) {
+    this.showMobileNav = !this.showMobileNav;
+    if( this.showMobileNav) {
+      navBar.classList.add("navbar-mobile");
+      navIcon.classList.remove("bi-list");
+      navIcon.classList.add("bi-x");
+    } else {
+      navBar.classList.remove("navbar-mobile", "bi-x");
+      navIcon.classList.add("bi-list");
+    }
+  }
 }
