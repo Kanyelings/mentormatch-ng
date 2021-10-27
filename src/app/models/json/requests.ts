5d20c35bce9f0ff457b96492fd4461d4824ff152
elroykanye <elroykanye@gmail.com>
2021-10-27 22:07:49 +0100

    added mentor and mentee request interfaces

---
+++ src/app/models/json/requests.ts
@@ -0,0 +1,13 @@
+export interface MentorRequest {
+  first_name: string;
+  second_name: string;
+  email: string;
+  gender: string;
+  phone_number: string;
+  wa_number: string;
+  department: string;
+}
+
+export interface MenteeRequest extends MentorRequest {
+  previous_school: string;
+}
