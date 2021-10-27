a66b1bcd6e99375763e448c8e029e1192ab3a434
elroykanye <elroykanye@gmail.com>
2021-10-27 22:08:19 +0100

    added consts for code and whatsapp link prefix

--- src/app/models/Constants.ts
+++ src/app/models/Constants.ts
@@ -2,3 +2,8 @@ export const MENTOR_MATCH_API: string = "https://mentors-match.herokuapp.com/api
 export const MENTEE_ENDPOINT: string = "/mentee"
 export const MENTOR_ENDPOINT: string = "/mentor"
 export const MATCH_ENDPOINT: string = "/match"
+
+export const CMR_CODE: string = "+237";
+export const WA_PREFIX: string = "https://wa.me/"
+
+
