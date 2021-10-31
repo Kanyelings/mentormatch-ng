c9980b8ff87e183add5f1992b38ff4f3fdb22a9b
elroykanye <elroykanye@gmail.com>
2021-10-31 18:00:36 +0100

    added supp for user loading via mmService: modified ./src/app/components/user/user-card/user-card.component.ts

--- src/app/components/user/user-card/user-card.component.ts
+++ src/app/components/user/user-card/user-card.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Mentee} from "../../../models/entity/mentee";
+import {Mentor} from "../../../models/entity/mentor";
+import {WA_PREFIX} from "../../../models/constants/endpoints";
 
 @Component({
   selector: 'app-user-card',
@@ -7,7 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserCardComponent implements OnInit {
 
-  constructor() { }
+  @Input() user: Mentee | Mentor;
+
+  constructor() {
+    this.user = {
+      department: "COME",
+      email: "elroykanye@gmail.com",
+      first_name: "Elroy",
+      gender: "M",
+      image_path: "",
+      level: "400",
+      phone_number: "237672270627",
+      previous_school: "GBHS Ndop",
+      second_name: "Kanye",
+      wa_number: WA_PREFIX.concat("237672270627"),
+      about: "I am crazy",
+    }
+  }
+
+  getUserName() : string {
+    return this.user.first_name.concat(" ").concat(this.user.second_name);
+  }
 
   ngOnInit(): void {
   }
