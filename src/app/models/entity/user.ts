37567a4195626d8efb156b78c529f36bd3e2c7fa
elroykanye <elroykanye@gmail.com>
2021-10-31 18:00:38 +0100

    added supp for user loading via mmService: modified ./src/app/models/entity/user.ts

--- src/app/models/entity/user.ts
+++ src/app/models/entity/user.ts
@@ -1,2 +1,13 @@
-export class User {
+
+export interface User {
+  id: number;
+  department: string;
+  email: string;
+  first_name: string;
+  gender: string;
+  image_path: string;
+  level: string;
+  phone_number: string;
+  second_name: string;
+  wa_number: string;
 }
