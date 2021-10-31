74b29ca16725014b7d1d8647c151b468c2f9b9a6
elroykanye <elroykanye@gmail.com>
2021-10-31 18:00:38 +0100

    added supp for user loading via mmService: modified ./src/app/app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import {MainHomeComponent} from "./components/home/main-home/main-home.component";
 import {ContactMainComponent} from "./components/contact/contact-main/contact-main.component";
 import {MmMainComponent} from "./components/mentor-mentee/mm-main/mm-main.component";
+import {MainUserComponent} from "./components/user/main-user/main-user.component";
 
 const routes: Routes = [
   {
     path: '', component: MainHomeComponent
+  },
+  {
+    path: 'users', component: MainUserComponent
   },
   {
     path: 'contact', component: ContactMainComponent
