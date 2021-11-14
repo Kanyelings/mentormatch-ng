43036172437050ebc2493316058d2906bec22383
elroykanye <elroykanye@gmail.com>
2021-11-14 13:04:26 +0100

    added matches component ./src/app/app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import {MainHomeComponent} from "./components/home/main-home/main-home.component
 import {ContactMainComponent} from "./components/contact/contact-main/contact-main.component";
 import {MmMainComponent} from "./components/mentor-mentee/mm-main/mm-main.component";
 import {MainUserComponent} from "./components/user/main-user/main-user.component";
+import {MatchesComponent} from "./components/mentor-mentee/matches/matches.component";
 
 const routes: Routes = [
   {
@@ -17,6 +18,9 @@ const routes: Routes = [
   },
   {
     path: 'apply', component: MmMainComponent
+  },
+  {
+    path: 'matches', component: MatchesComponent
   },
 ];
 
