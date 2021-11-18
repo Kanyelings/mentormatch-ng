37da66e36a8c924a7294ff852c0f18fb5d9ac2d8
elroykanye <elroykanye@gmail.com>
2021-11-18 21:21:29 +0100

    fixed up home counts load

--- src/app/components/home/home-counts/home-counts.component.ts
+++ src/app/components/home/home-counts/home-counts.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {MmService} from "../../../services/mm.service";
 import {Mentor} from "../../../models/entity/mentor";
 import {Mentee} from "../../../models/entity/mentee";
+import {Match} from "../../../models/entity/match";
 
 @Component({
   selector: 'app-home-counts',
@@ -12,40 +13,37 @@ export class HomeCountsComponent implements OnInit {
 
   mentorCount: number = 0;
   menteeCount: number = 0;
+  matchCount: number = 0;
   counts: Counts;
 
   constructor(private mmService: MmService) {
-    this.counts = {
-      mentors: 0,
-      mentees: 0,
-      total: 0,
-      matches: 0
-    };
+    this.counts = {mentors: "", mentees: "", total: "", matches: ""};
   }
 
   ngOnInit(): void {
-    this.mmService.getAllMs("mentor")
-      .subscribe((mentors: Mentor[]) => {
-        this.mentorCount = mentors.length;
-      });
-    this.mmService.getAllMs("mentee")
-      .subscribe((mentees: Mentee[]) => {
-        this.menteeCount = mentees.length;
-      })
+    this.mmService.getAllMs("mentor").subscribe((mentors: Mentor[]) => {
+      this.mentorCount = mentors.length;
+    });
+    this.mmService.getAllMs("mentee").subscribe((mentees: Mentee[]) => {
+      this.menteeCount = mentees.length;
+    });
+    this.mmService.getAllMatches().subscribe((matches: Match[]) => {
+      this.matchCount = matches.length;
+    });
 
     this.counts = {
-      mentors: this.mentorCount,
-      mentees: this.menteeCount,
-      total: this.mentorCount + this.menteeCount,
-      matches: 0 // TODO update this to persist the front with valid data
+      mentors: this.mentorCount.toString(),
+      mentees: this.menteeCount.toString(),
+      total: (this.mentorCount + this.menteeCount).toString(),
+      matches: this.matchCount.toString() // TODO update this to persist the front with valid data
     }
   }
 
 }
 
 interface Counts {
-  mentors: number;
-  mentees: number;
-  total: number;
-  matches: number;
+  mentors: string;
+  mentees: string;
+  total: string;
+  matches: string;
 }
