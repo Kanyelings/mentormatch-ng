7d30818de0620fc0b5119e00fcb047cea7d3a693
elroykanye <elroykanye@gmail.com>
2021-11-18 21:00:37 +0100

    added service method for loading individual user

--- src/app/services/mm.service.ts
+++ src/app/services/mm.service.ts
@@ -5,10 +5,10 @@ import {Mentor} from "../models/entity/mentor";
 import {Observable, throwError} from "rxjs";
 import {
   MATCH_ENDPOINT_ALL,
-  MENTEE,
+  MENTEE, MENTEE_ENDPOINT,
   MENTEE_ENDPOINT_ADD,
   MENTEE_ENDPOINT_ALL,
-  MENTOR,
+  MENTOR, MENTOR_ENDPOINT,
   MENTOR_ENDPOINT_ADD,
   MENTOR_ENDPOINT_ALL,
   MENTOR_MATCH_API
@@ -43,21 +43,19 @@ export class MmService {
   }
 
   public getAllMs(role: string): Observable<any> {
-    let endpoint: string = "";
+    let endpoint: string;
     switch (role) {
-      case MENTOR:
-        endpoint = MENTOR_ENDPOINT_ALL;
-        break;
-      case MENTEE:
-        endpoint = MENTEE_ENDPOINT_ALL;
-        break;
-      default:
-        endpoint = MENTOR_MATCH_API;
+      case MENTOR: endpoint = MENTOR_ENDPOINT_ALL; break;
+      case MENTEE: endpoint = MENTEE_ENDPOINT_ALL; break;
+      default: endpoint = MENTOR_MATCH_API;
     }
 
     return this.http.get<any>(
       endpoint,
       this.httpHeader
+    ).pipe(
+      retry(1),
+      catchError(this.processError)
     );
   }
 
@@ -65,11 +63,30 @@ export class MmService {
     return this.http.get<any>(
       MATCH_ENDPOINT_ALL,
       this.httpHeader
+    ).pipe(
+      retry(1),
+      catchError(this.processError)
+    );
+  }
+
+  public getUser(id: string, role: string): Observable<any> {
+    let endpoint: string;
+    switch (role) {
+      case "mentor": endpoint = MENTOR_ENDPOINT; break;
+      case "mentee": endpoint = MENTEE_ENDPOINT; break;
+      default: endpoint = MENTOR_MATCH_API;
+    }
+    return this.http.get<any>(
+      endpoint,
+      this.httpHeader
+    ).pipe(
+      retry(1),
+      catchError(this.processError)
     );
   }
 
   processError(err: { error: { message: string; }; status: any; message: any; }): Observable<never> {
-    let message = "";
+    let message: string;
     if(err.error instanceof ErrorEvent) {
       message = err.error.message
     } else {
